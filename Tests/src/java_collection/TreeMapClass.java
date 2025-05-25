87e78448a5885f0f89026b014e17f11da07c1484
NileshDhawale01 <n.dhawale@rnt.ai>
2025-05-25 23:03:41 +0530

    Tree Map

--- Tests/src/java_collection/TreeMapClass.java
+++ Tests/src/java_collection/TreeMapClass.java
@@ -147,7 +147,7 @@ public class TreeMapClass {
 		treeMap.merge(100, "If Present Updates", (key, val) -> val = "Merge Update");
 		System.out.println("Merge :: " + treeMap);
 
-// 10.		clear()
+// 10.		clear() 
 		treeMap.clear();
 		System.out.println("Clear :: " + treeMap);
 	}
